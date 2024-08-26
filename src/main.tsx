e61b27f3ab619a753bc64f29f70ec188ce591243
Aliaksei Dobroczynny <amsure00dev@gmail.com>
2024-08-26 23:20:22 +0200

    update config due to repo name

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://amsuredev.github.io/counter-front-end/tonconnect-manifest.json';
+const manifestUrl = 'https://amsuredev.github.io/first_contract_front_end/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
